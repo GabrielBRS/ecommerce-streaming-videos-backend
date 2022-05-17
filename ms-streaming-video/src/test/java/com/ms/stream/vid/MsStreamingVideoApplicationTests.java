ed20b72032865175f72176a45828613d8a2987dc
Gabriel Sousa <gabriel.brs.gsousa@gmail.com>
2022-05-17 14:13:23 -0300

    frist commit

---
+++ ms-streaming-video/src/test/java/com/ms/stream/vid/MsStreamingVideoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ms.stream.vid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsStreamingVideoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
