678f59faedfd7dbb7afe3babab2bceb6fa4a7a29
Gabriel Sousa <gabriel.brs.gsousa@gmail.com>
2022-05-23 14:23:00 -0300

    Ajuste Microsserviço.

--- hr-payroll/src/main/java/com/gabrielsousa/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/com/gabrielsousa/hrpayroll/HrPayrollApplication.java
@@ -4,12 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@LoadBalancerClient(name="hr-worker")
-@EnableDiscoveryClient
+@EnableEurekaClient
 @EnableFeignClients
 @SpringBootApplication
+//Se ouver mais instâncias do microserviço é nessário usar o Ribbon 
+//ou LoadBalancerClient
+
+//@LoadBalancerClient(name="hr-worker")
+//@EnableDiscoveryClient
+
 public class HrPayrollApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HrPayrollApplication.class, args);
